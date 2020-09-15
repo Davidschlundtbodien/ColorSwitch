daf54d7fbd504779b144e0a1f7fcdeaf6b88007d
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-09-15 15:43:55 -0600

    adds sound when scoring a point as well as animated play button.

--- ColorSwitch/GameScene.swift
+++ ColorSwitch/GameScene.swift
@@ -120,6 +120,7 @@ extension GameScene: SKPhysicsContactDelegate {
         if contactMask == PhysicsCatagories.ballCatagory | PhysicsCatagories.switchCatagory {
             if let ball = contact.bodyA.node?.name == "Ball" ? contact.bodyA.node as? SKSpriteNode : contact.bodyB.node as? SKSpriteNode{
                 if currentColorIndex == switchState.rawValue {
+                    run(SKAction.playSoundFileNamed("bling", waitForCompletion: false))
                     score += 1
                     updateScoreLabel()
                     ball.run(SKAction.fadeOut(withDuration: 0.25), completion: {ball.removeFromParent()
